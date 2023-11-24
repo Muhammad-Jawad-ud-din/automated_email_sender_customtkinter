9e90c5e28fa8e0a5bcb71fe42c2161c80a3145dd
Aamir Khan Maarofi <kaamir6600@gmail.com>
2023-11-24 21:55:59 +0500

    Three columns layout is set

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import customtkinter
+from ButtonsBar import ButtonsBar 
+from MiddleFame import MiddleFrame
+from FilesFrame import FilesFrame
 
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
 class App(customtkinter.CTk):
-    def __init__(self):
+    def __init__(self, title, size):
         super().__init__()
-        self.geometry("980x500")
+        self.geometry(f"{size[0]}x{size[1]}")
+        self.title(title)
+        # self.resizable(False, False) # User can't change the windows size
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure((0, 1, 2), weight=4)
+
+
         
-        # User can't change the windows size
-        self.minsize(980, 500)  
-        self.maxsize(980, 500)
+        self.buttonsBar = ButtonsBar(master=self)
+        self.buttonsBar.grid(row=0, column=0, ipadx=50, padx=10, pady=10, sticky="nsew")
+        
+        self.buttonsBar1 = MiddleFrame(master=self)
+        self.buttonsBar1.grid(row=0, column=1, ipadx=220, pady=10, sticky="nsew")
 
-        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
-        self.button.pack(padx=20, pady=20)
-
-    def button_callbck(self):
-        print("button clicked")
-
+        self.buttonsBar2 = FilesFrame(master=self)
+        self.buttonsBar2.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")        
+        
+        # self.buttonsBar = ButtonsBar(master=self)
+        # self.buttonsBar.grid(row=0, column=0, padx=(0, 500), sticky="nsew")
 
 if __name__ == "__main__":
-    app = App()
+    app = App("Automated Email Sender", (980, 500))
     app.mainloop()
