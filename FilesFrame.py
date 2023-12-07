96e3920b929b01f3f67b5b829ef5ab8dac3220be
Aamir Khan Maarofi <kaamir6600@gmail.com>
2023-12-08 01:55:04 +0500

    Deleted Unnecessary Files

--- FilesFrame.py
+++
@@ -1,6 +0,0 @@
-import customtkinter
-from CTkXYFrame import * 
-
-class FilesFrame(customtkinter.CTkScrollableFrame): # CTkXYFrame
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
